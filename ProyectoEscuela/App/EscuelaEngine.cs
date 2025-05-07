93773a473e993dd274eedbff65c76d978b9bac35
German Bartoli <bartoligerman497@gmail.com>
2025-05-06 22:40:19 -0300

    Update EscuelaEngine.cs
    Clase 28
    
    Refactorización de Métodos en C# para Eficiencia de Código

--- ProyectoEscuela/App/EscuelaEngine.cs
+++ ProyectoEscuela/App/EscuelaEngine.cs
@@ -1,6 +1,7 @@
 ﻿using ProyectoEscuela.Entidades;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ProyectoEscuela.App
@@ -18,6 +19,57 @@ namespace ProyectoEscuela.App
         {
             Escuela = new Escuela("Nombre de la escuela", 2000, TipoEscuela.Primaria, ciudad: "Córdoba", pais: "Argentina");
 
+            CargarCursos();
+
+            foreach (var curso in Escuela.lstCursos)
+            {
+                curso.lstAlumnos.AddRange(CargarAlumnos());
+
+            }
+
+            CargarAsignaturas();
+
+            CargarEvaluaciones();
+
+        }
+
+        private void CargarEvaluaciones()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CargarAsignaturas()
+        {
+            foreach (var curso in Escuela.lstCursos)
+            {
+                List<Asignatura> lstAsignaturas = new List<Asignatura>()
+                {
+                    new Asignatura{Nombre = "Matemáticas"},
+                    new Asignatura{Nombre = "Educación Física"},
+                    new Asignatura{Nombre = "Castellano"},
+                    new Asignatura{Nombre = "Ciencias Naturales"},
+                };
+                curso.lstAsignaturas.AddRange(lstAsignaturas);
+            }
+        }
+
+        private IEnumerable<Alumno> CargarAlumnos()
+        {
+            string[] nombre1 = { "nombre1", "nombre2" };
+            string[] nombre2 = { "nombre1", "nombre2" };
+            string[] apellido1 = { "apellido1", "apellido2" };
+
+            // Producto cartesiano
+            var lstAlumnos = from n1 in nombre1
+                             from n2 in nombre2
+                             from a1 in apellido1
+                             select new Alumno { Nombre= $"{n1} {n2} {a1}" };
+            return lstAlumnos;
+            
+        }
+
+        private void CargarCursos()
+        {
             Escuela.lstCursos = new List<Curso>();
 
             Escuela.lstCursos.Add(new Curso { Nombre="101", Jornada = TipoJornada.Mañana });
